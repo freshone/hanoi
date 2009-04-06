724f245927294904e59f82a2e47b90690b8c7f27
Jeremy McCarthy <mccarthy.jeremy@gmail.com>
2009-04-05 23:43:22 -0700

    first commit

---
+++ hanoi.rb
@@ -0,0 +1,46 @@
+################################################################################
+# Tower of Hanoi Problem
+# Ruby Learning Excersize
+#
+# by Jeremy D. McCarthy
+# 2009/04/05
+################################################################################
+
+class Hanoi
+	attr_accessor :numDisks, :a, :b, :c
+	@count
+
+	def initialize
+		numDisks = 9
+		@a = (1..numDisks).to_a.sort.reverse
+		@b, @c = Array.new(numDisks), Array.new(numDisks)
+	end
+
+	def start
+		@count = 0
+		process()
+	end
+	
+	def process(num = @a.length, from = @a, dest = @c, remain = @b)
+		if(num == 1)
+			dest.push(from.pop)
+			system("clear")
+			@count += 1
+			printPegs()
+			sleep(1)
+		else
+			process(num-1, from, remain, dest)
+			process(1, from, dest, remain)
+			process(num-1, remain, dest, from)
+		end
+	end
+
+	def printPegs
+		print "A:]" + @a.to_s + "\n"
+		print "B:]" + @b.to_s + "\n"
+		print "C:]" + @c.to_s + "\n\n"
+		puts "Number of operations: " + @count.to_s
+	end
+end
+
+Hanoi.new.start
